bb57f9af7a166f76003d4937c753183375326c15
supipi99 <supipi@sutratechnologies.com>
2025-01-16 17:38:43 +0530

    fontEndchenges

---
+++ src/components/StoryFeed.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react'; 
+import axios from 'axios'; 
+ 
+const StoryFeed = () => { 
+  const [stories, setStories] = useState([]); 
+ 
+  
+  return ( 
+    <div> 
+    </div> 
+  ); 
+}; 
+ 
+export default StoryFeed; 
